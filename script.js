e0910b9d0f5743fc3b681baee680cedfcdca2e0f
Kimberly MB <k.mueses@yahoo.com>
2023-03-10 20:32:40 -0500

    Sets up & styles custom video controls

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ menu.addEventListener('click',()=>{
   menu.classList.toggle('change')
 })
 // End of Navbar
+
+// Section 2 Video
+const video = document.querySelector('.video')
+const btn = document.querySelector('.buttons i')
+const bar = document.querySelector('.video-bar')
+
+const playPause = ()=> {
+  if (video.paused) {
+    video.play()
+    btn.className = 'fa-regular fa-circle-pause'
+  } else {
+    video.pause()
+    btn.className = 'fa-regular fa-circle-play'
+  }
+}
+
+
+// End of Section 2 Video
